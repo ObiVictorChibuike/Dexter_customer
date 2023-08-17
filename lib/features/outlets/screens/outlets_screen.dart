3418997d279221c27a4429585e33c346466c46eb
Okwor Chukwuemeka Nnajiofor <thenaijadev@gmail.com>
2023-08-17 13:22:39 +0100

    Working on form

--- lib/features/outlets/screens/outlets_screen.dart
+++ lib/features/outlets/screens/outlets_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:nettapp/core/app_colors/app_colors.dart';
-import 'package:nettapp/core/widgets/text_widget.dart';
 import 'package:nettapp/features/auth/widgets/app_bar_row.dart';
 import 'package:nettapp/features/auth/widgets/input_field_widget.dart';
-import 'package:nettapp/features/outlets/widgets/outlet_item.dart';
+import 'package:nettapp/features/outlets/widgets/outlets_widget.dart';
 
 class OutletsScreen extends StatelessWidget {
   const OutletsScreen({super.key});
@@ -55,39 +53,6 @@ class _OutletListState extends State<OutletList> {
               labelColor: Colors.black),
         ),
         const OutletsWidget()
-      ],
-    );
-  }
-}
-
-class OutletsWidget extends StatelessWidget {
-  const OutletsWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-          margin: const EdgeInsets.only(top: 50, bottom: 20),
-          decoration: const BoxDecoration(color: AppColors.blue),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextWidget(
-                text: "Outlet News",
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-              TextWidget(
-                text: "Last Visited",
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ],
-          ),
-        ),
-        const OutletItem()
       ],
     );
   }
---
+++ lib/features/outlets/widgets/outlets_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:nettapp/core/app_colors/app_colors.dart';
+import 'package:nettapp/core/widgets/text_widget.dart';
+import 'package:nettapp/features/outlets/widgets/outlet_item.dart';
+
+class OutletsWidget extends StatelessWidget {
+  const OutletsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+          margin: const EdgeInsets.only(top: 50, bottom: 20),
+          decoration: const BoxDecoration(color: AppColors.blue),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextWidget(
+                text: "Outlet News",
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              TextWidget(
+                text: "Last Visited",
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ],
+          ),
+        ),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem(),
+        const OutletItem()
+      ],
+    );
+  }
+}
