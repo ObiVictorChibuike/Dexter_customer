5651e55933acbeb46a8ac7f18cec4ecba847d18f
Okwor Chukwuemeka Nnajiofor <thenaijadev@gmail.com>
2023-08-17 13:30:17 +0100

    Working on form

--- lib/features/outlets/screens/outlets_screen.dart
+++ lib/features/outlets/screens/outlets_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nettapp/core/app_colors/app_colors.dart';
 import 'package:nettapp/features/auth/widgets/app_bar_row.dart';
-import 'package:nettapp/features/auth/widgets/input_field_widget.dart';
-import 'package:nettapp/features/outlets/widgets/outlets_widget.dart';
+import 'package:nettapp/features/outlets/widgets/outlet_list.dart';
 
 class OutletsScreen extends StatelessWidget {
   const OutletsScreen({super.key});
@@ -18,42 +18,14 @@ class OutletsScreen extends StatelessWidget {
           OutletList()
         ],
       ),
-    );
-  }
-}
-
-class OutletList extends StatefulWidget {
-  const OutletList({super.key});
-
-  @override
-  State<OutletList> createState() => _OutletListState();
-}
-
-class _OutletListState extends State<OutletList> {
-  final GlobalKey<FormFieldState> key_1 = GlobalKey<FormFieldState>();
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: InputFieldWidget(
-              label: "",
-              hintText: "Search for outlet",
-              onChanged: (val) {},
-              textFieldkey: key_1,
-              fillColor: Colors.transparent,
-              suffixIcon: Transform.scale(
-                scale: .4,
-                child: Image.asset(
-                  "assets/images/search_icon.png",
-                  width: 10,
-                ),
-              ),
-              labelColor: Colors.black),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.blue,
+        child: const Icon(
+          Icons.filter_list,
+          size: 30,
         ),
-        const OutletsWidget()
-      ],
+        onPressed: () {},
+      ),
     );
   }
 }
---
+++ lib/features/outlets/widgets/outlet_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:nettapp/features/auth/widgets/input_field_widget.dart';
+import 'package:nettapp/features/outlets/widgets/outlets_widget.dart';
+
+class OutletList extends StatefulWidget {
+  const OutletList({super.key});
+
+  @override
+  State<OutletList> createState() => _OutletListState();
+}
+
+class _OutletListState extends State<OutletList> {
+  final GlobalKey<FormFieldState> key_1 = GlobalKey<FormFieldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: InputFieldWidget(
+              label: "",
+              hintText: "Search for outlet",
+              onChanged: (val) {},
+              textFieldkey: key_1,
+              fillColor: Colors.transparent,
+              suffixIcon: Transform.scale(
+                scale: .4,
+                child: Image.asset(
+                  "assets/images/search_icon.png",
+                  width: 10,
+                ),
+              ),
+              labelColor: Colors.black),
+        ),
+        const OutletsWidget()
+      ],
+    );
+  }
+}
