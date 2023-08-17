c13a456235f78d53e45c444462f8ce84a9155938
Okwor Chukwuemeka Nnajiofor <thenaijadev@gmail.com>
2023-08-17 16:46:23 +0100

    Working on form

--- lib/features/trade_visit/widgets/outlet_details.dart
+++ lib/features/trade_visit/widgets/outlet_details.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:nettapp/features/trade_visit/widgets/outlet_details_row.dart';
 
 class OutletDetails extends StatelessWidget {
   const OutletDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [OutletDetailsRow(label: "Outlet name", value: "Wines et al")],
+    );
   }
 }
